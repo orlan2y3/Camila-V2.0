91daa8832094614d7b4afaed6758746c0e4f19f6
orlan2y3 <orlandoreynoso1983@gmail.com>
2021-04-22 22:07:53 -0400

    update

--- Facturacion_Inventario/Formularios/Factura.vb
+++ Facturacion_Inventario/Formularios/Factura.vb
@@ -1386,8 +1386,4 @@ Public Class Form1
             btbuscar.PerformClick()
         End If
     End Sub
-
-    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
-
-    End Sub
 End Class
